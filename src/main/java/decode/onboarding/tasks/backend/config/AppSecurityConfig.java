6a69194be8ca4d68cc385b328d69d4c9f7aea520
bvidakovic <bono.vidakovic@protonmail.com>
2019-08-07 11:49:17 +0200

    Dev profile enabled

--- src/main/java/decode/onboarding/tasks/backend/config/AppSecurityConfig.java
+++ src/main/java/decode/onboarding/tasks/backend/config/AppSecurityConfig.java
@@ -1,27 +1,28 @@
 package decode.onboarding.tasks.backend.config;
 
-import decode.onboarding.tasks.backend.service.impl.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
+@Profile("!dev")
 @RequiredArgsConstructor
+@EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsServiceImpl userDetailsServiceImpl;
+    private final PasswordEncoder passwordEncoder;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsServiceImpl)
-                .passwordEncoder(passwordEncoder());
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
     }
 
     @Override
@@ -30,11 +31,6 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                 authorizeRequests().anyRequest().authenticated()
                 .and()
                 .httpBasic();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
 }
---
+++ src/main/java/decode/onboarding/tasks/backend/config/AppSecurityConfigDev.java
@@ -0,0 +1,20 @@
+package decode.onboarding.tasks.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Profile("dev")
+@Configuration
+public class AppSecurityConfigDev extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.
+                authorizeRequests().anyRequest().permitAll()
+                .and()
+                .csrf().disable()
+                .headers().frameOptions().disable();
+    }
+}
